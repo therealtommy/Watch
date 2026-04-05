60cf4e6745f252f123d935e4e449ce6fcdb31a3a
therealtommy <sokolovai1887@ya.ru>
2026-04-05 12:56:17 +0700

    Prepare app for MVVW refactoring

--- app/src/main/java/com/example/watch/data/MovieRepository.kt
+++ app/src/main/java/com/example/watch/data/MovieRepository.kt
@@ -2,14 +2,22 @@ package com.example.watch.data
 
 import com.example.watch.db.MovieDao
 import com.example.watch.model.Movie
+import com.example.watch.model.OmdbSearchResponse
+import com.example.watch.network.OmdbApi
 import kotlinx.coroutines.flow.Flow
 
-class MovieRepository(private val movieDao: MovieDao) {
+class MovieRepository(
+    private val movieDao: MovieDao,
+    private val omdbApi: OmdbApi,
+    private val apiKey: String
+) {
     fun getAllWatchlist(): Flow<List<Movie>> = movieDao.getAll()
 
     suspend fun addMovie(movie: Movie) = movieDao.insert(movie)
 
-    suspend fun removeMovie(movie: Movie) = movieDao.delete(movie)
-
     suspend fun removeMoviesByIds(ids: List<String>) = movieDao.deleteByIds(ids)
+
+    suspend fun searchMovies(query: String, year: String?): OmdbSearchResponse {
+        return omdbApi.searchMovies(apiKey, query, year)
+    }
 }
